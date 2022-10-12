7f5813b7493496f73fb9e831ca02dae93df17d95
André Jonas <andrejonaspesceromanoli@gmail.com>
2022-10-12 15:30:24 -0300

    Inserindo arquivo

---
+++ algoritimos/ex4.py
@@ -0,0 +1,12 @@
+import re
+
+
+# texto='Hello word'
+
+def fraseInvertida(texto):
+    invertido=texto[::-1]
+    return invertido
+
+print(fraseInvertida('Hello word'))
+
+print(fraseInvertida('Ola Mund0'))
