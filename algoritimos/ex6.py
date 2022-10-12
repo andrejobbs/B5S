4c4417ef6cddf9addb6b92fed42263c8b76cb55c
André Jonas <andrejonaspesceromanoli@gmail.com>
2022-10-12 19:34:30 -0300

    Inserindo arquivos

---
+++ algoritimos/ex6.py
@@ -0,0 +1,13 @@
+def ordenaArray():
+    lista=[5,4,2,6,8,10,1]
+    lista.sort()
+    print(lista)
+    # nv_lista=[]
+    # for x in lista:
+    #     ordenador=x
+    #     if ordenador<x:
+    #         nv_lista.append(ordenador)
+    # print(nv_lista)
+
+ordenaArray()
+
---
+++ algoritimos/ex7.py
@@ -0,0 +1,9 @@
+def primeiroValorNaoRepetido():
+    lista=[1,1,2,3,4,2,4,5,5,2,6]
+    nv_lista=[]
+    for x in lista:
+        if not x.len(2)==lista :
+            nv_lista.append(x)
+    print(nv_lista)
+
+primeiroValorNaoRepetido()
