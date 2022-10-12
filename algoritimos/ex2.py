9b25bdb65f3b4912944aa1ae2bb47fdff9e5b570
André Jonas <andrejonaspesceromanoli@gmail.com>
2022-10-12 13:20:19 -0300

    Adicionando exercicio

---
+++ algoritimos/ex2.py
@@ -0,0 +1,12 @@
+def mediaAritmetica():
+        entrada=[60,70,90]
+        if (len(entrada)==3):
+            media=sum(entrada)/len(entrada)
+            print(media)
+        elif (len(entrada)!=3):
+            media=False
+            print(media)
+mediaAritmetica()
+
+
+
